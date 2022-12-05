864645e301c09fd9068f62c411953b6020468502
Marcelo Morgado <cmarcelom@gmail.com>
2022-12-05 21:33:55 +0000

    Improve typings

---
+++ contracts/mock/Import.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+contract Import {
+    // Note: Import contracts you want to generate typechain files
+}
--- test/MyLpWallet.test.ts
+++ test/MyLpWallet.test.ts
@@ -7,10 +7,12 @@ import {
   MyLpWallet,
   IUniswapV2Router02,
   IUniswapV2Router02__factory,
+  IUniswapV2Factory,
+  IUniswapV2Factory__factory,
+  IUniswapV2Pair__factory,
   MyToken__factory,
   MyLpWallet__factory,
 } from "../typechain-types";
-import { Contract } from "ethers";
 import { Address } from "../helper";
 
 const { UNISWAP_V2_ROUTER02, UNISWAP_V2_FACTORY, WETH } = Address;
@@ -20,7 +22,7 @@ describe("MyLpWallet", () => {
   let myLpWallet: MyLpWallet;
   let myToken: MyToken;
   let uniswapRouter: IUniswapV2Router02;
-  let uniswapFactory: Contract;
+  let uniswapFactory: IUniswapV2Factory;
 
   beforeEach(async () => {
     [wallet] = await ethers.getSigners();
@@ -29,11 +31,7 @@ describe("MyLpWallet", () => {
     myToken = MyToken__factory.connect(MyToken.address, wallet);
     myLpWallet = MyLpWallet__factory.connect(MyLpWallet.address, wallet);
     uniswapRouter = IUniswapV2Router02__factory.connect(UNISWAP_V2_ROUTER02, wallet);
-    uniswapFactory = new ethers.Contract(
-      UNISWAP_V2_FACTORY,
-      ["function getPair(address,address) view returns (address)"],
-      wallet
-    );
+    uniswapFactory = IUniswapV2Factory__factory.connect(UNISWAP_V2_FACTORY, wallet);
 
     await myToken.mint(parseEther("1000"));
     await myToken.approve(myLpWallet.address, ethers.constants.MaxUint256);
@@ -43,11 +41,7 @@ describe("MyLpWallet", () => {
     const tx = await myLpWallet.addLiquidity(parseEther("100"), { value: parseEther("10") });
     expect(tx).to.emit(myLpWallet, "LiquidityAdded");
     const pairAddress = await uniswapFactory.getPair(myToken.address, WETH);
-    const uniswapPair = new ethers.Contract(
-      pairAddress,
-      ["function totalSupply() view returns (uint256)", "function balanceOf(address) view returns (uint256)"],
-      wallet
-    );
+    const uniswapPair = IUniswapV2Pair__factory.connect(pairAddress, wallet);
     const balance = await uniswapPair.balanceOf(myLpWallet.address);
 
     // @ts-ignore
